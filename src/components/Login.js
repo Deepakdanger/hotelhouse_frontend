0af56e92cb914e35dc838aa85f0288fee9340759
Deepakdanger <depk.96.kumar@gmail.com>
2021-11-29 17:32:01 +0530

    Create login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
-  const a = 4;
+    const [state, setState] = useState({ user: '', password: '' });
+
+    const handleUserChange = (e) => {
+        setState({ ...state, user: e.target.value });
+    };
+    
+    const handlePassChange = (e) => {
+        setState({ ...state, password: e.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submitted')
+        console.log(e);
+      };
+
+
   return (
-    <div className="category_list swing">
-      homePage{a} Login
-    </div>
+    <div className="login_page">
+      <div className="login_new"> LOGIN </div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input className="input_user" type="text" id="title" placeholder="UserName" value={state.title} onChange={(e) => handleUserChange(e)} />
+        <input className="input_password" type="text" id="title" placeholder="Password" value={state.title} onChange={(e) => handlePassChange(e)} />
+        <button className="submit_login" type="submit">Submit</button>
+      </form>
+    </div>    
   );
 };
 
