9d17f07b04ac29cbe975485c1dfe98ee81357330
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-02 19:04:51 +0530

    Add proptype to housefulldetails

--- src/components/Housefulldetail.js
+++ src/components/Housefulldetail.js
@@ -34,23 +34,17 @@ const HouseFullDetail = ({ ele }) => {
 
 HouseFullDetail.propTypes = {
   ele: PropTypes.shape({
-    strMeal: PropTypes.string,
-    strCategory: PropTypes.string,
-    strYoutube: PropTypes.string,
-    strArea: PropTypes.string,
-    strInstructions: PropTypes.string,
-    strMealThumb: PropTypes.string,
+    title: PropTypes.string,
+    cost: PropTypes.string,
+    location: PropTypes.string,
   }),
 };
 
 HouseFullDetail.defaultProps = {
   ele: PropTypes.shape({
-    strMeal: '1',
-    strCategory: 'fhfh',
-    strYoutube: '1',
-    strArea: 'fhfh',
-    strInstructions: '1',
-    strMealThumb: '1',
+    title: 'fhfh',
+    cost: '1',
+    location: 'fhfh',
   }),
 };
 
