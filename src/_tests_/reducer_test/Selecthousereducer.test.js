3e31a0d9b99668b6d6ecafdc6451a4de67b82fb4
Deepakdanger <depk.96.kumar@gmail.com>
2021-12-18 20:50:31 +0530

    Add test for selected house reducer

---
+++ src/_tests_/reducer_test/Selecthousereducer.test.js
@@ -0,0 +1,20 @@
+import SelectedHouseReducer from '../../reducers/Selecthousereducer';
+import { SELECTED_HOUSE } from '../../actions/index';
+
+describe('SelectedHouseReducer function', () => {
+  it('should return the initial state', () => {
+    const action = { type: 'dummy_action' };
+    const initialState = '';
+    expect(SelectedHouseReducer(undefined, action)).toEqual(initialState);
+  });
+
+  it('should handle SELECT_CATEGORY', () => {
+    const action = {
+      type: SELECTED_HOUSE,
+      ele: { id: '1' },
+    };
+    const expectedState = '1';
+    expect(SelectedHouseReducer({}, action) === '').toBe(false);
+    expect(SelectedHouseReducer({}, action)).toEqual(expectedState);
+  });
+});
